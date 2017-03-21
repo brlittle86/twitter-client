f785a628b064f01af937899b43d150f2afb1dfdf
Brandon Little <brlittle86@gmail.com>
2017-03-20 17:01:54 -0700

    Did the entire lab assignment. Something about setting up a singleton to hold tweets, then populating the table cells with tweet data.

---
+++ Twitter Client/Twitter Client/Tweets.swift
@@ -0,0 +1,41 @@
+//
+//  Tweets.swift
+//  Twitter Client
+//
+//  Created by Brandon Little on 3/20/17.
+//  Copyright © 2017 Brandon Little. All rights reserved.
+//
+
+import Foundation
+
+class Tweets {
+    
+    static let shared = Tweets()
+    
+    var allTweets = [Tweet]()
+    
+    private init(){}
+    
+    func add(tweet: Tweet){
+        self.allTweets.append(tweet)
+    }
+    
+    func remove(tweet: Tweet){
+        self.allTweets = self.allTweets.filter{ (item) -> Bool in
+            return item.id != tweet.id
+        }
+    }
+    
+    func removeAll(){
+        self.allTweets.removeAll()
+    }
+    
+    func getTweetAt(index: Int) -> Tweet{
+        return self.allTweets[index]
+    }
+    
+    func count() -> Int{
+        return self.allTweets.count
+    }
+    
+}
